da8928fa95910e43bb1dcaa1f546f4264da3da41
Liuyi616 <lion61ly@gmail.com>
2021-01-28 13:06:49 -0800

    1.28

--- routes/hello.js
+++ routes/hello.js
@@ -3,8 +3,15 @@
  * GET home page.
  */
 
+// exports.view = function(req, res){
+//   res.render('index', {
+//   	'name': '',
+//   });
+// };
+
 exports.view = function(req, res){
-  res.render('index', {
-  	'name': '',
+	var nameToShow = req.params.userName;
+  res.render("hello", {
+  	"name": nameToShow,
   });
 };
